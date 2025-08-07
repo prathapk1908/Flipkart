84c7eae6b7c02643cf3d473a424e7705b91d0fd8
prathapk1908 <prathapkannan1908@gmail.com>
2025-08-08 01:22:30 +0530

    fist version of flipkart using react

---
+++ src/FlipkartComponents/FlipkartProducts.js
@@ -0,0 +1,53 @@
+import camera from './images/cam.webp';
+import bag from './images/bags.webp';
+import cycle from './images/cycle.webp';
+import heater from './images/heater.webp';
+import slipper from './images/slipper.webp';
+import { Link } from "react-router-dom";
+export default function FlipkartProducts(){
+    const products = [
+        {
+            image : camera,
+            model : 'Top MirrorLess Cameras',
+            availableProducts:'Canon, Sony, Fujifilm...'
+        } ,{
+            image : bag,
+            model : 'School, College & Travel',
+            availableProducts:'Canon, Sony, Fujifilm...'
+        } ,{
+            image : cycle,
+            model : 'MTB Bicycle without Gear',
+            availableProducts:'Canon, Sony, Fujifilm...'
+        } ,{
+            image : slipper,
+            model : 'Liberty, Khadim & more',
+            availableProducts:'Canon, Sony, Fujifilm...'
+        },
+        {
+            image : heater,
+            model : 'Liberty, Khadim & more',
+            availableProducts:'Canon, Sony, Fujifilm...'
+        }
+    ]
+
+    return(
+        <div className='product-container row'>
+            
+                {
+                    products.map(function(pdatas){
+                        return(
+                            <div className='col container-fluid mx-2'>
+                                 <Link className='link' to={'/notfound'}>
+                                 <img src={pdatas.image} alt='failed'/>
+                                 </Link>
+                                <h6>{pdatas.model}</h6>
+                                <p className='shopnow'>Show Now!</p>
+                                <p className='p-avail'>{pdatas.availableProducts}</p>
+                            </div>
+                        )
+                    })
+                }
+            
+        </div>
+    )
+}
